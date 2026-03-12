3ac98c23624450f6f75bbd8c4b5b5c4888595f88
therealdanmalmx <danmalmx@gmail.com>
2026-03-12 21:42:50 +0100

    Add update and delete

--- API/Controllers/MenuItemController.cs
+++ API/Controllers/MenuItemController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTO.MenuItem;
 using Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -53,6 +54,51 @@ namespace API.Controllers
             await _dbContext.SaveChangesAsync();
 
             return Created("/api/menuitem", newItem);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateMenuItem(UpdateMenuItemDTO updateMenutItem, Guid id)
+        {
+            var menuItemToUpdate = await _dbContext.MenuItems.FindAsync(id);
+
+            if (menuItemToUpdate is null)
+            {
+                return BadRequest($"No menu item with id {id} exists");
+            }
+
+            if (!string.IsNullOrWhiteSpace(updateMenutItem.Name))
+            {
+                menuItemToUpdate.Name = updateMenutItem.Name;
+            }
+            if (!string.IsNullOrWhiteSpace(updateMenutItem.Description))
+            {
+                menuItemToUpdate.Description = updateMenutItem.Description;
+            }
+            if (updateMenutItem.Price != 0.0m)
+            {
+                menuItemToUpdate.Price = updateMenutItem.Price;
+            }
+
+            _dbContext.MenuItems.Update(menuItemToUpdate);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(menuItemToUpdate);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMenuItem(Guid id)
+        {
+            var menuItemToDelete = await _dbContext.MenuItems.FindAsync(id);
+
+            if (menuItemToDelete is null)
+            {
+                return BadRequest($"Could not find a menut item with id {id}");
+            }
+
+            _dbContext.MenuItems.Remove(menuItemToDelete);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
