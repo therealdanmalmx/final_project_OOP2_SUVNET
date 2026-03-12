82378869a12d2ec96da786dccc31e07b439e6fb9
therealdanmalmx <danmalmx@gmail.com>
2026-03-12 18:32:22 +0100

    Add Card component for restaurants

---
+++ Client/DTO/GetRestaurantsDTO.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.DTO
+{
+    public class GetRestaurantsDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public int MyProperty { get; set; }
+        public float Review { get; set; }
+        public string OpeningHours { get; set; }
+        public decimal MinimumOrderValue { get; set; }
+        public decimal ServiceFee { get; set; }
+    }
+}
