4cd61913009ce596ba61b9bc742617c74fb5ed75
therealdanmalmx <danmalmx@gmail.com>
2026-03-12 21:42:59 +0100

    Remove required

--- API/Models/MenuItem.cs
+++ API/Models/MenuItem.cs
@@ -8,8 +8,8 @@ namespace Models
     public class MenuItem
     {
         public Guid Id { get; set; } = Guid.CreateVersion7();
-        public required string Name { get; set; }
-        public required string Description { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
         public decimal Price { get; set; }
         public Guid RestaurantId { get; set; }
     }
