0d4341e7820cfda65fde7c5f6b6427fb1a538e2c
therealdanmalmx <danmalmx@gmail.com>
2026-03-13 19:42:06 +0100

    Add account controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTO.Account;
+using API.Models;
+using Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+
+        public AccountController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNewAccount(CreateAccountDTO newAccount)
+        {
+            if(newAccount is null)
+            {
+                return BadRequest("Account can't be empty");
+            }
+
+            var account = new Account
+            {
+                Name = newAccount.Name,
+                Address = newAccount.Address,
+                Phone = newAccount.Phone,
+                Email = newAccount.Email,
+                Role = newAccount.Role
+            };
+
+            Courier? courier = null;
+
+            if (newAccount.Role == Role.Courier)
+            {
+                courier = new Courier
+                {
+                    AccountId = newAccount.Id,
+                    IsAvailable = courier!.IsAvailable
+                };
+            }
+
+            _dbContext.Accounts.Add(account);
+            await _dbContext.SaveChangesAsync();
+
+            return Created($"/api/account", account);
+        }
+    }
+}
