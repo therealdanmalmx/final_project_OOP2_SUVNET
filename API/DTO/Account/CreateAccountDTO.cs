b5b927e9d8b8d9f856a06b3b3dceb730bc2bef2e
therealdanmalmx <danmalmx@gmail.com>
2026-03-13 19:41:55 +0100

    Add DTO

---
+++ API/DTO/Account/CreateAccountDTO.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+
+namespace API.DTO.Account
+{
+    public class CreateAccountDTO
+    {
+
+        public required Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Address { get; set; }
+        public required string Email { get; set; }
+        public required string Phone { get; set; }
+        public required Role Role { get; set; }
+    }
+}
