a67ced157f5b3547949a4e149fac69866823da68
therealdanmalmx <danmalmx@gmail.com>
2026-03-13 20:32:43 +0100

    Add account nd courier create dto

--- API/DTO/Account/CreateAccountDTO.cs
+++ API/DTO/Account/CreateAccountDTO.cs
@@ -8,8 +8,6 @@ namespace API.DTO.Account
 {
     public class CreateAccountDTO
     {
-
-        public required Guid Id { get; set; }
         public required string Name { get; set; }
         public required string Address { get; set; }
         public required string Email { get; set; }
---
+++ API/DTO/Courier/CreateCourierDTO.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTO.Courier
+{
+    public class CreateCourierDTO
+    {
+        public Guid AccountId { get; set; }
+        public Models.Account Account { get; set; }
+        public bool IsAvailable { get; set; }
+        public List<Models.Order> OrderDeliveries { get; set; }
+    }
+}
