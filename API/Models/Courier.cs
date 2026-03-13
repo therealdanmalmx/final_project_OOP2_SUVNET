1376d5d234b9cb9cabd0ab464ff1d6301a2267f3
therealdanmalmx <danmalmx@gmail.com>
2026-03-13 20:43:53 +0100

    Clean up

--- API/Models/Courier.cs
+++ API/Models/Courier.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Migrations;
-
 namespace API.Models
 {
     public class Courier
     {
         public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
         public bool IsAvailable { get; set; }
         public List<Order> OrderDeliveries { get; set; }
     }
