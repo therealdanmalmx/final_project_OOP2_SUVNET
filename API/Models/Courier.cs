1aa3a777c16735f9a388e87ac78f65ba9ff6fd54
therealdanmalmx <danmalmx@gmail.com>
2026-03-13 20:15:42 +0100

    Update courier model

--- API/Models/Courier.cs
+++ API/Models/Courier.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Migrations;
 
 namespace API.Models
 {
     public class Courier
     {
+        [Key]
         public Guid AccountId { get; set; }
+        public Account Account { get; set; }
         public bool IsAvailable { get; set; }
         public List<Order> OrderDeliveries { get; set; }
     }
