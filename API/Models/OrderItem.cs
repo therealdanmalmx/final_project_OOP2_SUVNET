a03b3ea97b9303b340643d28753b601c2818e7f6
therealdanmalmx <danmalmx@gmail.com>
2026-03-14 13:28:53 +0100

    Add DTO for update status

--- API/Models/OrderItem.cs
+++ API/Models/OrderItem.cs
@@ -2,7 +2,6 @@ namespace API.Models
 {
     public class OrderItem
     {
-        public Guid Id { get; set; } = Guid.CreateVersion7();
         public required string Name { get; set; }
         public required decimal Price { get; set; }
         public required int Quantity { get; set; }
