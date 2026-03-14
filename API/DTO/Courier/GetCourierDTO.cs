b6ee67ea379f3709ad76a1e6e5abf8861b434fbb
therealdanmalmx <danmalmx@gmail.com>
2026-03-14 16:49:40 +0100

    Add GetCourier DTO

---
+++ API/DTO/Courier/GetCourierDTO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTO.Courier
+{
+    public class GetCourierDTO
+    {
+        public string Name { get; set; } = string.Empty;
+        public bool IsAvailable { get; set; }
+        public List<Models.Order>? OrderDeliveries { get; set; }
+    }
+}
