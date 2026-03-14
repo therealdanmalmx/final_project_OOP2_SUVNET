896594126d81eb9d02c19bb72db71ef989310d9b
therealdanmalmx <danmalmx@gmail.com>
2026-03-14 18:23:40 +0100

    Addd Image to getRestaurantDTO

--- API/DTO/Restuarant/GetRestaurantsDTO.cs
+++ API/DTO/Restuarant/GetRestaurantsDTO.cs
@@ -7,6 +7,7 @@ namespace DTO
 {
     public class GetRestaurantsDTO
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Address { get; set; }
@@ -15,5 +16,7 @@ namespace DTO
         public string OpeningHours { get; set; }
         public decimal MinimumOrderValue { get; set; }
         public decimal ServiceFee { get; set; }
+        public string Image { get; set; }
+
     }
 }
