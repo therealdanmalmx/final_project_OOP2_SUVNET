e81b0c6bc66883c825c4c15c7208b229387ece33
therealdanmalmx <danmalmx@gmail.com>
2026-03-17 08:33:11 +0100

    Use the mealPlan API for menu item pics

---
+++ Client/Models/MealList.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Models
+{
+    public class MealList
+    {
+        public List<Meals> Meals { get; set; } = [];
+    }
+}
---
+++ Client/Models/Meals.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Models
+{
+    public class Meals
+    {
+        public string strMeal { get; set; }
+        public string strMealThumb { get; set; }
+        public string idMeal { get; set; }
+
+    }
+}
