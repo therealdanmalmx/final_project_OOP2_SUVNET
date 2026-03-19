b58242711d80312b3b0d5063eabfd3be3094b487
therealdanmalmx <danmalmx@gmail.com>
2026-03-19 21:40:19 +0100

    harge

---
+++ API/Migrations/20260319134631_AddDeliveryChargeToRestaurant.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeliveryChargeToRestaurant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DeliveyCharge",
+                table: "Restaurants",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveyCharge",
+                table: "Restaurants");
+        }
+    }
+}
--- Client/Services/OrderStateService.cs
+++ Client/Services/OrderStateService.cs
@@ -1,6 +1,4 @@
-using API.DTO.Order;
-using API.DTO.OrderItem;
-using API.Models;
+
 using Client.DTO;
 using Client.Models;
 
@@ -8,7 +6,6 @@ namespace Client.Services
 {
     public class OrderStateService
     {
-        // This holds the object you want to pass
         public GetRestaurantsDTO? Restaurant { get; set; }
         public GetMenuItemDTO? MenuItem { get; set; }
         public CreateOrderDTO? Order { get; set; }
