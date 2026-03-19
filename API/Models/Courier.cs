996f67689e74c26dee5e4765417988e75f9fffc9
therealdanmalmx <danmalmx@gmail.com>
2026-03-19 21:38:15 +0100

    Courier constructor

--- API/Models/Courier.cs
+++ API/Models/Courier.cs
@@ -2,9 +2,15 @@ namespace API.Models
 {
     public class Courier
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.CreateVersion7();
         public string Name { get; set; } = string.Empty;
         public bool IsAvailable { get; set; }
         public List<Order>? Orders { get; set; }
+
+        public Courier(string name, bool isAvailable)
+        {
+            Name = name;
+            IsAvailable = isAvailable;
+        }
     }
 }
