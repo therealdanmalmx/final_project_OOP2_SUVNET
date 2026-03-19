042f1c6eeabc3aec747c636d37334ebc639191bf
therealdanmalmx <danmalmx@gmail.com>
2026-03-19 21:39:41 +0100

    Adjust CreateOrderDto

--- Client/DTO/CreateOrderDTO.cs
+++ Client/DTO/CreateOrderDTO.cs
@@ -1,19 +1,24 @@
 using System.ComponentModel.DataAnnotations;
+using API.Models;
 
-namespace API.DTO.Order
+namespace Client.DTO
 {
     public class CreateOrderDTO
     {
-        public Guid Id { get; set; } = Guid.CreateVersion7();
+        public Guid Id { get; set; }
         [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Address is required")]
         public string Address { get; set; }
+        public int Number { get; set; }
         [Required(ErrorMessage = "Phone is required")]
         public string Phone { get; set; }
         public string? Email { get; set; }
         public string? Instructions { get; set; }
+        public Status Status { get; set; }
         public Guid? CourierId { get; set; }
         public Guid? AccountId { get; set; }
+        public List<OrderItem> OrderItems { get; private set; } = [];
+
     }
 }
