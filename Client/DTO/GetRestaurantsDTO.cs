07be5216bb5516f6fa346a10cdd2f723b96fb329
therealdanmalmx <danmalmx@gmail.com>
2026-03-23 14:31:13 +0100

    Update type for opening hours

--- Client/DTO/GetRestaurantsDTO.cs
+++ Client/DTO/GetRestaurantsDTO.cs
@@ -11,7 +11,9 @@ namespace Client.DTO
         public string Address { get; set; }
         public int MyProperty { get; set; }
         public float Review { get; set; }
-        public string OpeningHours { get; set; }
+        public TimeOnly Opens { get; set; }
+        public TimeOnly Closes { get; set; }
+        public TimeOnly OrderCutOffTime { get; set; }
         public decimal MinimumOrderValue { get; set; }
         public decimal DeliveyCharge { get; set; }
         public decimal ServiceFee { get; set; }
