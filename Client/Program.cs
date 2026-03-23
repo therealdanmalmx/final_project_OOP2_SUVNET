0644e0c990e89a3618926eaa4e0a15eba16f1e34
therealdanmalmx <danmalmx@gmail.com>
2026-03-23 14:39:54 +0100

    Add Blazor Local storage

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,14 +3,17 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
 using BlazorBlueprint.Components;
 using Client.Services;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5095") });
 
 builder.Services.AddBlazorBlueprintComponents();
 builder.Services.AddScoped<OrderStateService>();
+
 
 await builder.Build().RunAsync();
