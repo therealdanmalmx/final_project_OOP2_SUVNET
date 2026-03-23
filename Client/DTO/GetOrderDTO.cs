1d9bd3b41ad42204ade212cb5f76c965191deca9
therealdanmalmx <danmalmx@gmail.com>
2026-03-23 22:02:37 +0100

    Add courier id to get order dto

--- Client/DTO/GetOrderDTO.cs
+++ Client/DTO/GetOrderDTO.cs
@@ -10,12 +10,13 @@ namespace Client.DTO
     public class GetOrderDTO
     {
         public Guid Id { get; init; }
-        public int Number { get; init; }
+        public int Number { get; set; }
         public string Name { get; init; } = string.Empty;
         public string Address { get; init;} = string.Empty;
         public string Phone { get; init; } = string.Empty;
         public string Email { get; init; } = string.Empty;
         public string Instructions { get; init;} = string.Empty;
+        public Guid? CourierId { get; set; }
         public Status Status { get;  init; }
         public List<GetOrderItemDTO> OrderItems { get; set; }
     }
