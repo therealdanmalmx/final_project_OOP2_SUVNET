676b06a683577fd7282d649db872ca2a95c93a37
therealdanmalmx <danmalmx@gmail.com>
2026-03-24 21:08:06 +0100

    remove required

--- Client/DTO/CreateOrderItemDTO.cs
+++ Client/DTO/CreateOrderItemDTO.cs
@@ -7,8 +7,8 @@ namespace Client.DTO
 {
     public class CreateOrderItemDTO
     {
-        public required string Name { get; set; }
-        public required decimal Price { get; set; }
-        public required int Quantity { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
     }
 }
