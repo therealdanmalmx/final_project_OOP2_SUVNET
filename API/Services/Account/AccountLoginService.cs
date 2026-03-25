6a4cede03e58fb61421cafba776bfdefbb8c8402
therealdanmalmx <danmalmx@gmail.com>
2026-03-25 16:41:54 +0100

    Remove duplication

--- API/Services/Account/AccountLoginService.cs
+++ API/Services/Account/AccountLoginService.cs
@@ -56,9 +56,5 @@ namespace API.Services.Account
             return new AccountLoginResponse(true, null, jwt);
         }
 
-        Task<AccountLoginResponse> IAccountLoginService.Login(AccountLoginRequest request)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
