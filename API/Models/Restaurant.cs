08db7ee4dbf01a0f2c637cf290180efb6e215c9d
therealdanmalmx <danmalmx@gmail.com>
2026-03-26 21:03:11 +0100

    Remve Guid initialization

--- API/Models/Restaurant.cs
+++ API/Models/Restaurant.cs
@@ -2,7 +2,7 @@ namespace API.Models
 {
     public class Restaurant
     {
-        public Guid Id { get; init; } = Guid.CreateVersion7();
+        public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Category { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
