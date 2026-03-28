99eb8b1f1d4f03ed2c996417d2e20ae8495f596b
therealdanmalmx <danmalmx@gmail.com>
2026-03-28 12:18:13 +0100

    Change type for udate status

--- API/DTO/Order/UpdateOrderStatusDTO.cs
+++ API/DTO/Order/UpdateOrderStatusDTO.cs
@@ -1,7 +1,9 @@
+using API.Models;
+
 namespace API.DTO
 {
     public class UpdateOrderStatusDTO
     {
-        public string Status { get; set; } = string.Empty;
+        public Status Status { get; set; }
     }
 }
