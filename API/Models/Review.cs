8129529351c308f9f31d771faa95c02d940c827d
therealdanmalmx <danmalmx@gmail.com>
2026-03-29 17:06:35 +0200

    Change type

--- API/Models/Review.cs
+++ API/Models/Review.cs
@@ -3,7 +3,7 @@ namespace API.Models
     public class Review
     {
         public Guid Id { get; set; }
-        public decimal Score { get; set; }
+        public float Score { get; set; }
         public string Comment { get; set; } = string.Empty;
         public Guid? OrderId { get; set; }
         public Guid? RestaurantId { get; set; }
