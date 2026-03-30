957629276b1ce3a8851e1252f4ab97130b556b5c
therealdanmalmx <danmalmx@gmail.com>
2026-03-30 22:35:36 +0200

    Models/DTO copies to client

---
+++ Client/DTO/AccountRegistrationRequest.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+
+namespace API.DTO
+{
+    public class AccountRegistrationRequest
+    {
+        [Required(ErrorMessage = "Namn är obligatoriskt")]
+        [StringLength(30, MinimumLength = 10,
+        ErrorMessage = "Namn måste vara minst 10 bokstävder långt")]
+        public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Address är obligatoriskt")]
+        [StringLength(50, MinimumLength = 10,
+        ErrorMessage = "Address måste vara minst 10 bokstävder långt")]
+        public string Address { get; set; } = string.Empty;
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Email är obligatoriskt"), EmailAddress(ErrorMessage = "Emailhar inte rätt format.")]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [StringLength(10, MinimumLength = 10,
+        ErrorMessage = "Telefonnummer måste vara 10 siffror långt")]
+        public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+        [Required, Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+        [Required]
+        public Role Role { get; set; }
+    }
+}
---
+++ Client/DTO/AccountRegistrationResponse.cs
@@ -0,0 +1,18 @@
+namespace Client.DTO
+{
+    public class AccountRegistrationResponse
+    {
+        public bool IsSuccessful { get; set; }
+        public IEnumerable<string>? Errors { get; set; }
+
+        public AccountRegistrationResponse(bool isSuccessful)
+        {
+            IsSuccessful = isSuccessful;
+        }
+        public AccountRegistrationResponse(bool isSuccessful, IEnumerable<string>? errors)
+        {
+            IsSuccessful = isSuccessful;
+            Errors = errors;
+    }
+    }
+}
