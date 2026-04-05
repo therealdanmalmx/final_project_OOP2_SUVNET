5f69d46e71b77d6e9ac5b4722a9eb3ee9c4d7b0a
therealdanmalmx <danmalmx@gmail.com>
2026-04-05 11:11:37 +0200

    Remove minimum order value migration

---
+++ API/Migrations/20260405091103_RemoveMinimumOrderValue.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveMinimumOrderValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MinimumOrderValue",
+                table: "Restaurants");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "MinimumOrderValue",
+                table: "Restaurants",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
