a0b4352f85c90ebc3afdfa3da18b393c96644851
therealdanmalmx <danmalmx@gmail.com>
2026-04-05 11:05:37 +0200

    List of reviews on restaurant migration

---
+++ API/Migrations/20260405084320_AddReviewListOnRestaurant.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReviewListOnRestaurant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_RestaurantId",
+                table: "Reviews",
+                column: "RestaurantId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Restaurants_RestaurantId",
+                table: "Reviews",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Restaurants_RestaurantId",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_RestaurantId",
+                table: "Reviews");
+        }
+    }
+}
